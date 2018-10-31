2512c255c5fefcc0886814ee1ea1dc298312c37e
Josh Colstrom <joshua.colstrom@mnsu.edu>
2018-10-31 21:29:03 +0000

    add employee functionality done, css needs help

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,15 +2,22 @@ class EmployeesController < ApplicationController
     def index
         @employees = Employee.all
     end
+   
+    def show
+        id = params[:id]
+        @employee = Employee.find(id)
+    end
     
     def new
         @employee = Employee.new
     end
     
     def create
-        @employee = Employee.create!(employee_params)
+        params.require(:employee)
+        permitted = params[:employee].permit(:address, :last_name, :first_name, :phone_number, :employee_type, :driver_id, :dob)
+        @employee = Employee.create!(permitted)
         flash[:notice] = "#{@employee.first_name} was successfully created."
-        redirect_to employee_path
+        redirect_to employees_path
     end
     
     def employee_params
