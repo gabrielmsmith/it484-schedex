39db98d52924a97146ab5afd448765775c0f1455
Jeremy Klein <jeremy.klein@mnsu.edu>
2018-11-06 04:05:24 +0000

    HomePage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/schedule'
+
+  get 'home/employees'
+
+  root 'home#index'
+
   resources :employees
-  root :to => redirect('/employees')
+  #root :to => redirect('/employees')
 end
