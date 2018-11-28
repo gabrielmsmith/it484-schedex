902656757a56c004fa5ff83717e0177f79f60cec
Gabriel Smith <gabriel.smith@mnsu.edu>
2018-11-28 08:13:39 +0000

    Adding a lot of stuff

---
+++ app/controllers/shifts_controller.rb
@@ -0,0 +1,20 @@
+class ShiftsController < ApplicationController
+  def index
+    @shifts = Shift.all
+  end
+  
+  def edit
+    id = params[:id]
+    @shift = Shift.find(id)
+    @employees = Employee.all
+  end
+  
+  def update
+    @shift = Shift.find params[:id]
+    params.require(:shift)
+    permitted = params[:shift].permit(:emp_id)
+    @shift.update_attributes!(permitted)
+    flash[:notice] = "#{Shift.get_employee_name(@shift.emp_id)} was assigned the shift."
+    redirect_to shifts_path
+  end
+end
